63935f9515c55759b721cf4c639f3c4ad29ab689
griesba <griesba.pat@hotmail.com>
2021-01-24 01:54:17 +0100

    Configure route to connect to services closes #2

---
+++ src/main/java/com/griesba/brewery/gateway/config/LocalhostRootConfig.java
@@ -0,0 +1,21 @@
+package com.griesba.brewery.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalhostRootConfig {
+    @Bean
+    RouteLocator localhostRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route("beer-service", r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/vi/beerUpc/*")//any path matching "/api/v1/beer/*" and "/api/vi/beerUpc/*"
+                        .uri("http://localhost:8080"))
+                .route("order-service", r -> r.path("/api/v1/customers/**")
+                        .uri("http://localhost:8081"))
+                .route("inventory-service", r -> r.path("/api/v1/beer/*/inventory")
+                        .uri("http://localhost:8082"))
+                .build();
+    }
+}
